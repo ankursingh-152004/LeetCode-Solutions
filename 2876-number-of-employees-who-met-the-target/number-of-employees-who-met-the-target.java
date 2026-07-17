e37cadf832e9c3b585320149a011a9092fe42007
Ankur Singh <myname.ankur.singh@gmail.com>
2026-07-17 20:52:25 +0530

    Time: 5 ms (1.07%) | Memory: 44.8 MB (8.26%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
@@ -1,8 +1,9 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
         int c=0;
+        Arrays.sort(hours);
         for(int i=0;i<hours.length;i++){
-            if(hours[i]>=target) c++;
+            if(hours[i]>=target) return hours.length-i;
         }
         return c;
     }
