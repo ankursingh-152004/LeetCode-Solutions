f898f150ad8d1029a4ab0273d8b2969cb116a6de
Ankur Singh <myname.ankur.singh@gmail.com>
2026-08-02 23:27:29 +0530

    Time: 1 ms (98.92%) | Memory: 46.3 MB (71.08%) - LeetSync

--- 4116-minimum-moves-to-equal-array-elements-iii/minimum-moves-to-equal-array-elements-iii.java
+++ 4116-minimum-moves-to-equal-array-elements-iii/minimum-moves-to-equal-array-elements-iii.java
@@ -1,7 +1,10 @@
 class Solution {
     public int minMoves(int[] nums) {
-     Arrays.sort(nums);
-     int largest=nums[nums.length-1];
+     
+     int largest=0;
+     for(int i=0;i<nums.length;i++){
+        largest=Math.max(largest,nums[i]);
+     }
      int moves=0;
      for(int i=0;i<nums.length;i++){
         moves+=Math.abs(nums[i]-largest);
