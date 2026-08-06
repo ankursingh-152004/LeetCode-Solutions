81d03eeacaa67f019c31caed5cc71c6222f45b0e
Ankur Singh <myname.ankur.singh@gmail.com>
2026-08-06 14:30:03 +0530

    Time: 3 ms (81.23%) | Memory: 43.7 MB (31.23%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        // ans.add(Integer.valueOf(operations[0]));
+        for(int i=0;i<operations.length;i++){
+            if(operations[i].equals("C")){
+                ans.remove(ans.size()-1);
+            }else if(operations[i].equals("D")){
+                ans.add(2*ans.get(ans.size()-1));
+            }else if(operations[i].equals("+")){
+                ans.add(ans.get(ans.size()-1)+ans.get(ans.size()-2));
+            }else{
+                ans.add(Integer.parseInt(operations[i]));
+            }
+        }
+        int s=0;
+        for(int n:ans){
+            s+=n;
+        }
+        return s;
+    }
+}
