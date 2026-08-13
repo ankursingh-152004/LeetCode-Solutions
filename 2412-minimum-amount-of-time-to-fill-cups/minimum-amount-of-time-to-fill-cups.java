fac32da39d6d534e7bd615db4ee4316790b408e6
Ankur Singh <myname.ankur.singh@gmail.com>
2026-08-13 16:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (62.70%) - LeetSync

---
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fillCups(int[] amount) {
+        int max=0;int sum=0;
+        for(int i:amount){
+            max=Math.max(max,i);
+            sum+=i;
+        }
+        return Math.max(max,(sum+1)/2);
+    }
+}
