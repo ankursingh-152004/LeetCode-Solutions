a00321ba67b41a126abb359156c47dff17f92f8e
Ankur Singh <myname.ankur.singh@gmail.com>
2026-09-02 19:24:05 +0530

    Time: 4 ms (11.71%) | Memory: 42.2 MB (97.75%) - LeetSync

--- 4126-minimum-number-of-flips-to-reverse-binary-string/minimum-number-of-flips-to-reverse-binary-string.java
+++ 4126-minimum-number-of-flips-to-reverse-binary-string/minimum-number-of-flips-to-reverse-binary-string.java
@@ -1,23 +1,36 @@
+// class Solution {
+//     public int minimumFlips(int n) {
+//         int count=0;
+//         String s="";
+//         while(n!=0){
+//             int d=n%2;
+//             s+=d;
+//             n/=2;
+//         }
+//         int a=0;
+//         int b=s.length()-1;
+//         while(a<b){
+//             if(s.charAt(a)!=s.charAt(b)){
+//                 count=count+2;
+               
+//             }
+//              a++;
+//                 b--;
+//         }
+//         return count;
+
+//     }
+// }
 class Solution {
     public int minimumFlips(int n) {
+        String res=Integer.toBinaryString(n);
+        String rev=new StringBuilder(res).reverse().toString();
         int count=0;
-        String s="";
-        while(n!=0){
-            int d=n%2;
-            s+=d;
-            n/=2;
-        }
-        int a=0;
-        int b=s.length()-1;
-        while(a<b){
-            if(s.charAt(a)!=s.charAt(b)){
-                count=count+2;
-               
+        for(int i=0;i<res.length();i++){
+            if(res.charAt(i)!=rev.charAt(i)){
+                count++;
             }
-             a++;
-                b--;
         }
         return count;
-
     }
 }
