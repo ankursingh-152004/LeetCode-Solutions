8345fa7a6ce7e7fa1675e11da3c0f5ceab10d446
Ankur Singh <myname.ankur.singh@gmail.com>
2026-09-19 17:09:17 +0530

    Time: 2 ms (98.68%) | Memory: 45 MB (47.28%) - LeetSync

---
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumStrongPairXor(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                if(Math.abs(nums[i]-nums[j])<=Math.min(nums[i],nums[j])){
+                    if(max<(nums[i]^nums[j])){
+                        max=nums[i]^nums[j];
+                    }
+                }
+            }
+        }
+        return max;
+    }
+}
